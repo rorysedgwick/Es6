5ebe3e7d22adad3bf2ed08940352f158b3491729
benjamin lees <benji.man.lees@gmail.com>
2015-08-23 17:02:28 +0100

    adding a problem

--- problems/1.js
+++ problems/1.js
@@ -1,26 +1,12 @@
 /*In the snippet of code below please replace all functions
  with arrows and then run the file using npm run babel-node */
- var obJ = {}
 
- function add1(number) {
-    number = number + 1;
-    return number;
- }
+var arr = [1,2,3]
 
- function multiplyBy5(number) {
-    number = number * 5;
-    return number;
- }
+function arrMap (arr) {
+   return arr.map( function (num) {
+      return num + 1;
+   })
+}
 
- function writeLine(obj) {
-   var number =  add1(1)
-   number = multiplyBy5(number);
-   var that = this;
-
-   obj.howOldAreYou(function () {
-     console.log('I am ' that.number + 'years old' )
-   });
- }
-
- writeLine(obj);
- obj.howOldAreYou();
+console.log(arrMap(arr))
